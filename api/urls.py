2b4e3d32cb9459ece5c22fcf5f46ebd302a8b044
ChrisMulya <chrismulyak@gmail.com>
2025-06-28 16:11:16 +0700

    v3

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from api import views
+from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'api'
 urlpatterns = [
-    path('provinsi/', views.ProvinsiListCreate.as_view(), name='provinsi-list-create'),
-    path('provinsi/<int:pk>/', views.ProvinsiDetail.as_view(), name='provinsi-detail'),
-    path('alatmusik/', views.AlatMusikListCreate.as_view(), name='alatmusik-list-create'),
-    path('alatmusik/<int:pk>/', views.AlatMusikDetail.as_view(), name='alatmusik-detail'),
+    # URL untuk Provinsi
+    path('provinsi/', views.ProvinsiListApiView.as_view(), name='provinsi-list'),
+    # URL untuk Alat Musik
+    path('alatmusik/', views.AlatMusikListApiView.as_view(), name='alatmusik-list'),
+    path('alatmusik/<int:id>/', views.AlatMusikDetailApiView.as_view(), name='alatmusik-detail'),
 ]
