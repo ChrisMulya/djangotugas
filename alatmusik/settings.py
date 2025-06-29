2bd95d2d6866046e2651a842b249d4eb734d373d
ChrisMulya <chrismulyak@gmail.com>
2025-06-29 20:29:58 +0700

    Update settings.py

--- alatmusik/settings.py
+++ alatmusik/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-9le%s#=#ruos!+p*+p!ykg^!wfj#zsrvcf+8oj8f%#npmrq$*g
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+APPEND_SLASH = False
 
 # settings.py
 ALLOWED_HOSTS = ['*']
