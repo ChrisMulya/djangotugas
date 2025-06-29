0161d656e89e039fdc372c7240d20e97f7404a20
ChrisMulya <chrismulyak@gmail.com>
2025-06-29 20:55:54 +0700

    Update settings.py

--- alatmusik/settings.py
+++ alatmusik/settings.py
@@ -89,12 +89,17 @@ WSGI_APPLICATION = 'alatmusik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=False) # ssl_require=False untuk Railway
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
