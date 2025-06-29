2440bc0ee355334fd4bdcd2f8b3cbbda46498db1
ChrisMulya <chrismulyak@gmail.com>
2025-06-29 19:11:10 +0700

    v8

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,26 @@
+# alatmusik_app/api/serializers.py
+
 from rest_framework import serializers
 from alatmusik_app.models import Provinsi, AlatMusik
 
 class AlatMusikSerializer(serializers.ModelSerializer):
-    provinsi = serializers.StringRelatedField()
+    # Field ini hanya untuk DIBACA (GET).
+    provinsi_nama = serializers.CharField(source='provinsi.nama', read_only=True)
 
     class Meta:
         model = AlatMusik
-        fields = ['id', 'nama', 'asal_daerah', 'deskripsi', 'provinsi']
+        fields = [
+            'id', 
+            'nama', 
+            'asal_daerah', 
+            'deskripsi', 
+            'provinsi',        # Field ini untuk MENULIS (POST/PUT), menerima ID.
+            'provinsi_nama'    # Field ini untuk MEMBACA (GET), menampilkan nama.
+        ]
+        extra_kwargs = {
+            'provinsi': {'write_only': True, 'required': True}
+        }
 
-# PERBARUI SERIALIZER INI
 class ProvinsiSerializer(serializers.ModelSerializer):
     alat_musik = AlatMusikSerializer(many=True, read_only=True)
 
