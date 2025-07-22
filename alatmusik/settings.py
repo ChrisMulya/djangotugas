9c0d660a4734548b92ccbde1c365ef7a14e3a2c8
ChrisMulya <chrismulyak@gmail.com>
2025-07-22 20:19:59 +0700

    sett

--- alatmusik/settings.py
+++ alatmusik/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'alatmusik_app',
     'api',
+    'corsheaders'
 
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,7 +75,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'alatmusik.wsgi.application'
+CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://web-production-0c0f.up.railway.app",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
